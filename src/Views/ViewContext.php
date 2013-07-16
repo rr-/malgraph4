9fbf7c52d728c989356f2186a1ccfeb4f3284951
Marcin Kurczewski <mkurczew@gmail.com>
2013-07-16 22:44:24 +0200

    Refurbished CSS, added missing images

--- src/Views/ViewContext.php
+++ src/Views/ViewContext.php
@@ -9,6 +9,11 @@ class ViewContext
 		$this->meta = new StdClass;
 		$this->meta->styles = [
 			'/media/css/core.css',
+			'/media/css/menu.css',
+			'/media/css/header.css',
+			'/media/css/glider.css',
+			'/media/css/icons.css',
+			'http://fonts.googleapis.com/css?family=Open+Sans|Ubuntu',
 		];
 		$this->meta->scripts = [
 			'http://code.jquery.com/jquery-1.10.2.min.js',
