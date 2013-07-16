9d5a7984f55e3c4bd4d14631283c9eb059c57461
Marcin Kurczewski <mkurczew@gmail.com>
2013-07-16 08:38:07 +0200

    View context remembers default stuff

--- public_html/dispatch.php
+++ public_html/dispatch.php
@@ -14,6 +14,7 @@ $classNames = array_filter(get_declared_classes(), function($className)
 });
 
 $controllerContext = new ControllerContext();
+$viewContext = new ViewContext();
 try
 {
 	$url = $_SERVER['REQUEST_URI'];
@@ -27,22 +28,19 @@ try
 		if ($className::parseRequest($url, $controllerContext))
 		{
 			Cache::beginSave($url);
-			$viewContext = new ViewContext();
 			$className::work($controllerContext, $viewContext);
 			View::render($viewContext);
 			Cache::endSave();
 			exit(0);
 		}
 	}
-	$viewContext = new ViewContext();
-	$viewContext->name = 'error-404';
+	$viewContext->viewName = 'error-404';
 	View::render($viewContext);
 }
 catch (Exception $e)
 {
 	#log error information
-	$viewContext = new ViewContext();
-	$viewContext->name = 'error';
+	$viewContext->viewName = 'error';
 	$viewContext->exception = $e;
 	View::render($viewContext);
 }
--- src/Views/ViewContext.php
+++ src/Views/ViewContext.php
@@ -2,4 +2,18 @@
 class ViewContext
 {
 	public $name;
+
+	public function __construct()
+	{
+		$this->renderStart = microtime(true);
+		$this->meta = new StdClass;
+		$this->meta->styles = [
+			'/media/css/core.css',
+		];
+		$this->meta->scripts = [
+			'http://code.jquery.com/jquery-1.10.2.min.js',
+			'/media/js/glider.js',
+			'/media/js/misc.js',
+		];
+	}
 }
