b5b6bf3840f0406dd393333a12ab2252052dd088
Marcin Kurczewski <mkurczew@gmail.com>
2013-07-19 20:32:48 +0200

    Closed #32

--- cron.php
+++ cron.php
@@ -1,37 +1,57 @@
 <?php
 require_once 'src/core.php';
 
-try
+if (isset($argv))
 {
+	$userNames = array_slice($argv, 1);
+}
+else
+{
+	$userNames = [];
 	$queue = new Queue(Config::$userQueuePath);
-	$userName = Config::$debugCron
-		? $queue->peek()
-		: $queue->dequeue();
-	if (empty($userName))
+	if (Config::$debugCron)
 	{
-		return;
+		$userNames []= $queue->peek();
 	}
-
-	$processor = new UserProcessor();
-	$context = $processor->process($userName);
-
-	$mediaProcessors =
-	[
-		Media::Anime => new AnimeProcessor(),
-		Media::Manga => new MangaProcessor()
-	];
-
-	foreach (R::findAll('usermedia', 'user_id = ?', [$context->userId]) as $userMedia)
+	else
 	{
-		if (!R::findOne('media', 'mal_id = ? AND media = ? AND processed >= DATETIME("now", "-21 days")', [$userMedia->mal_id, $userMedia->media]))
+		for ($i = 0; $i < 5; $i ++)
 		{
-			echo sprintf('Processing %s #%d' . PHP_EOL, Media::toString($userMedia->media), $userMedia->mal_id);
-			$mediaProcessors[$userMedia->media]->process($userMedia->mal_id);
+			$userNames []= $queue->dequeue();
 		}
 	}
 }
-catch (Exception $e)
+$userNames = array_filter($userNames);
+if (empty($userNames))
 {
-	Logger::log(Config::$errorLogPath, $e);
-	echo $e . PHP_EOL;
+	exit(0);
+}
+
+$userProcessor = new UserProcessor();
+$mediaProcessors =
+[
+	Media::Anime => new AnimeProcessor(),
+	Media::Manga => new MangaProcessor()
+];
+
+foreach ($userNames as $userName)
+{
+	try
+	{
+		printf('Processing user %s' . PHP_EOL, $userName);
+		$context = $userProcessor->process($userName);
+		foreach (R::findAll('usermedia', 'user_id = ?', [$context->userId]) as $userMedia)
+		{
+			if (!R::findOne('media', 'mal_id = ? AND media = ? AND processed >= DATETIME("now", "-21 days")', [$userMedia->mal_id, $userMedia->media]))
+			{
+				printf('Processing %s #%d' . PHP_EOL, Media::toString($userMedia->media), $userMedia->mal_id);
+				$mediaProcessors[$userMedia->media]->process($userMedia->mal_id);
+			}
+		}
+	}
+	catch (Exception $e)
+	{
+		Logger::log(Config::$errorLogPath, $e);
+		echo $e . PHP_EOL;
+	}
 }
