rr-/malgraph4

commit cf9ed4a5e1a58498554711da0d172ab8f4ae0375

GetText() accepts ViewContext

src/ControllerModules/AbstractUserControllerModule.php
@@ -4,7 +4,7 @@ abstract class AbstractUserControllerModule extends AbstractControllerModule
 	/**
 	* This method returns the text that is rendered in the menu in layout.
 	*/
-	public static function getText($media)
+	public static function getText(ViewContext $viewContext)
 	{
 		throw new UnimplementedException();
 	}

src/ControllerModules/UserControllerAchievementsModule.php
@@ -1,7 +1,7 @@
 <?php
 class UserControllerAchievementsModule extends AbstractUserControllerModule
 {
-	public static function getText($media)
+	public static function getText(ViewContext $viewContext)
 	{
 		return 'Achievements';
 	}

src/ControllerModules/UserControllerFavoritesModule.php
@@ -1,7 +1,7 @@
 <?php
 class UserControllerFavoritesModule extends AbstractUserControllerModule
 {
-	public static function getText($media)
+	public static function getText(ViewContext $viewContext)
 	{
 		return 'Favorites';
 	}

src/ControllerModules/UserControllerHistoryModule.php
@@ -1,7 +1,7 @@
 <?php
 class UserControllerHistoryModule extends AbstractUserControllerModule
 {
-	public static function getText($media)
+	public static function getText(ViewContext $viewContext)
 	{
 		return 'History';
 	}

src/ControllerModules/UserControllerListsModule.php
@@ -1,9 +1,9 @@
 <?php
 class UserControllerListsModule extends AbstractUserControllerModule
 {
-	public static function getText($media)
+	public static function getText(ViewContext $viewContext)
 	{
-		return ucfirst(Media::toString($media) . ' list');
+		return ucfirst(Media::toString($viewContext->media) . ' list');
 	}
 
 	public static function getUrlParts()

src/ControllerModules/UserControllerProfileModule.php
@@ -1,7 +1,7 @@
 <?php
 class UserControllerProfileModule extends AbstractUserControllerModule
 {
-	public static function getText($media)
+	public static function getText(ViewContext $viewContext)
 	{
 		return 'Profile';
 	}

src/ControllerModules/UserControllerRatingsModule.php
@@ -1,7 +1,7 @@
 <?php
 class UserControllerRatingsModule extends AbstractUserControllerModule
 {
-	public static function getText($media)
+	public static function getText(ViewContext $viewContext)
 	{
 		return 'Ratings';
 	}

src/ControllerModules/UserControllerSuggestionsModule.php
@@ -1,7 +1,7 @@
 <?php
 class UserControllerSuggestionsModule extends AbstractUserControllerModule
 {
-	public static function getText($media)
+	public static function getText(ViewContext $viewContext)
 	{
 		return 'Suggestions';
 	}

commit 598850352fd2db603d5cc8a2146b8acfa79900cf

Fixed media switching issue introduced in cf9ed4a5e1a58498554711da0d172ab8f4ae0375

src/ControllerModules/AbstractUserControllerModule.php
@@ -4,7 +4,7 @@ abstract class AbstractUserControllerModule extends AbstractControllerModule
 	/**
 	* This method returns the text that is rendered in the menu in layout.
 	*/
-	public static function getText(ViewContext $viewContext)
+	public static function getText(ViewContext $viewContext, $media)
 	{
 		throw new UnimplementedException();
 	}

src/ControllerModules/UserControllerAchievementsModule.php
@@ -1,7 +1,7 @@
 <?php
 class UserControllerAchievementsModule extends AbstractUserControllerModule
 {
-	public static function getText(ViewContext $viewContext)
+	public static function getText(ViewContext $viewContext, $media)
 	{
 		return 'Achievements';
 	}

src/ControllerModules/UserControllerFavoritesModule.php
@@ -1,7 +1,7 @@
 <?php
 class UserControllerFavoritesModule extends AbstractUserControllerModule
 {
-	public static function getText(ViewContext $viewContext)
+	public static function getText(ViewContext $viewContext, $media)
 	{
 		return 'Favorites';
 	}

src/ControllerModules/UserControllerHistoryModule.php
@@ -1,7 +1,7 @@
 <?php
 class UserControllerHistoryModule extends AbstractUserControllerModule
 {
-	public static function getText(ViewContext $viewContext)
+	public static function getText(ViewContext $viewContext, $media)
 	{
 		return 'History';
 	}

src/ControllerModules/UserControllerListsModule.php
@@ -1,9 +1,9 @@
 <?php
 class UserControllerListsModule extends AbstractUserControllerModule
 {
-	public static function getText(ViewContext $viewContext)
+	public static function getText(ViewContext $viewContext, $media)
 	{
-		return ucfirst(Media::toString($viewContext->media) . ' list');
+		return ucfirst(Media::toString($media) . ' list');
 	}
 
 	public static function getUrlParts()

src/ControllerModules/UserControllerProfileModule.php
@@ -1,7 +1,7 @@
 <?php
 class UserControllerProfileModule extends AbstractUserControllerModule
 {
-	public static function getText(ViewContext $viewContext)
+	public static function getText(ViewContext $viewContext, $media)
 	{
 		return 'Profile';
 	}

src/ControllerModules/UserControllerRatingsModule.php
@@ -1,7 +1,7 @@
 <?php
 class UserControllerRatingsModule extends AbstractUserControllerModule
 {
-	public static function getText(ViewContext $viewContext)
+	public static function getText(ViewContext $viewContext, $media)
 	{
 		return 'Ratings';
 	}

src/ControllerModules/UserControllerSuggestionsModule.php
@@ -1,7 +1,7 @@
 <?php
 class UserControllerSuggestionsModule extends AbstractUserControllerModule
 {
-	public static function getText(ViewContext $viewContext)
+	public static function getText(ViewContext $viewContext, $media)
 	{
 		return 'Suggestions';
 	}

src/Views/user-menu.phtml
@@ -7,7 +7,7 @@
 				<li>
 			<?php endif ?>
 				<a class="waitable" href="<?php echo UserControllerProfileModule::url($viewContext->user->name) ?>">
-					<?php echo UserControllerProfileModule::getText($viewContext) ?>
+					<?php echo UserControllerProfileModule::getText($viewContext, null) ?>
 					<i class="icon-profile"></i>
 				</a>
 			</li>
@@ -26,8 +26,8 @@
 							<li>
 						<?php endif ?>
 							<a class="waitable" href="<?php echo $module::url($viewContext->user->name, $media) ?>">
-								<?php echo $module::getText($viewContext) ?>
-								<i class="icon-<?php $x = explode(' ', strtolower($module::getText($viewContext))); echo end($x) ?>"></i>
+								<?php echo $module::getText($viewContext, $media) ?>
+								<i class="icon-<?php $x = explode(' ', strtolower($module::getText($viewContext, $media))); echo end($x) ?>"></i>
 							</a>
 						</li>
 					<?php endif ?>