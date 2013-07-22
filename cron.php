d7164562f31293cf43b06d6cb6e48b4dac6ff05c
Marcin Kurczewski <mkurczew@gmail.com>
2013-07-22 15:20:57 +0200

    Closed #48

--- cron.php
+++ cron.php
@@ -39,6 +39,11 @@ foreach ($userNames as $userName)
 	try
 	{
 		printf('Processing user %s' . PHP_EOL, $userName);
+		if (R::getAll('SELECT 0 FROM user WHERE LOWER(name) = LOWER(?) AND processed >= DATETIME("now", "-1 days")', [$userName]))
+		{
+			echo 'Too soon' . PHP_EOL;
+			continue;
+		}
 		$context = $userProcessor->process($userName);
 		foreach (R::getAll('SELECT um.mal_id, um.media FROM usermedia um LEFT OUTER JOIN media m ON um.mal_id = m.mal_id AND um.media = m.media WHERE um.user_id = ? AND (m.id IS NULL OR m.processed <= DATETIME("now", "-21 days"))', [$context->user->id]) as $row)
 		{
