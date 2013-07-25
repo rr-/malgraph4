8f7ea89204c95522756a2d975af7e90e240ca0d9
Marcin Kurczewski <mkurczew@gmail.com>
2013-07-25 23:21:22 +0200

    Added missing file

---
+++ src/ModelUtils/Distributions/MediaCreatorDistribution.php
@@ -0,0 +1,71 @@
+<?php
+class MediaCreatorDistribution extends AbstractDistribution
+{
+	public function getNullGroupKey()
+	{
+		return 0;
+	}
+
+	public static function fromEntries(array $entries = [])
+	{
+		$dist = new self();
+		if (!empty($entries))
+		{
+			R::begin();
+			R::exec('CREATE TEMPORARY TABLE hurr (media_id INTEGER)');
+			foreach (array_chunk(array_map(function($entry) { return $entry->media_id; }, $entries), Config::$maxDbBindings) as $chunk)
+			{
+				R::exec('INSERT INTO hurr VALUES ' . join(',', array_fill(0, count($chunk), '(?)')), $chunk);
+			}
+			switch (reset($entries)->media)
+			{
+				case Media::Anime:
+					$table = 'animeproducer';
+					break;
+				case Media::Manga:
+					$table = 'mangaauthor';
+					break;
+				default:
+					throw new BadMediaException();
+			}
+			$data = R::getAll('SELECT * FROM ' . $table . ' mc INNER JOIN hurr ON mc.media_id = hurr.media_id');
+			R::rollback();
+
+			$map = [];
+			foreach ($entries as $entry)
+			{
+				$entry->creators = [];
+				$map[$entry->media_id] = $entry;
+			}
+
+			foreach ($data as $row)
+			{
+				$row = ReflectionHelper::arrayToClass($row);
+				if (!isset($map[$row->media_id]))
+				{
+					continue;
+				}
+				if (BanHelper::isCreatorBanned($map[$row->media_id]->media, $row->mal_id))
+				{
+					continue;
+				}
+				$map[$row->media_id]->creators []= $row;
+			}
+
+			foreach ($map as $entry)
+			{
+				$dist->addEntry($entry);
+			}
+		}
+		$dist->finalize();
+		return $dist;
+	}
+
+	protected function addEntry($entry)
+	{
+		foreach ($entry->creators as $creator)
+		{
+			$this->addToGroup($creator, $entry);
+		}
+	}
+}
