0f677cc44393ac0513e92026c7f522c700a8f094
Marcin Kurczewski <mkurczew@gmail.com>
2013-07-27 12:13:44 +0200

    Nicer code

--- cron.php
+++ cron.php
@@ -25,16 +25,23 @@ foreach ($userNames as $userName)
 	try
 	{
 		printf('Processing user %s' . PHP_EOL, $userName);
-		if (R::getAll('SELECT 0 FROM user WHERE LOWER(name) = LOWER(?) AND processed >= DATETIME("now", "-1 days")', [$userName]))
+		$sql = 'SELECT 0 FROM user WHERE LOWER(name) = LOWER(?)' .
+			' AND processed >= DATETIME("now", "-1 days")';
+		if (R::getAll($sql, [$userName]))
 		{
 			echo 'Too soon' . PHP_EOL;
 			continue;
 		}
 		$context = $userProcessor->process($userName);
-		foreach (R::getAll('SELECT um.mal_id, um.media FROM usermedia um LEFT OUTER JOIN media m ON um.mal_id = m.mal_id AND um.media = m.media WHERE um.user_id = ? AND (m.id IS NULL OR m.processed <= DATETIME("now", "-21 days"))', [$context->user->id]) as $row)
+		$sql = 'SELECT um.mal_id, um.media FROM usermedia um' .
+			' LEFT OUTER JOIN media m ON um.mal_id = m.mal_id AND um.media = m.media' .
+			' WHERE um.user_id = ?' .
+			' AND (m.id IS NULL OR m.processed <= DATETIME("now", "-21 days"))';
+		foreach (R::getAll($sql, [$context->user->id]) as $row)
 		{
-			printf('Processing %s #%d' . PHP_EOL, Media::toString($row['media']), $row['mal_id']);
-			$mediaProcessors[$row['media']]->process($row['mal_id']);
+			$row = ReflectionHelper::arrayToClass($row);
+			printf('Processing %s #%d' . PHP_EOL, Media::toString($row->media), $row->mal_id);
+			$mediaProcessors[$row->media]->process($row->mal_id);
 		}
 	}
 	catch (BadProcessorKeyException $e)
