fe476a661200772826fd6e70d94d1d05d96c35a1
Marcin Kurczewski <mkurczew@gmail.com>
2013-07-30 14:42:31 +0200

    Fixed rare problem with downloading

--- src/Processors/AbstractProcessor.php
+++ src/Processors/AbstractProcessor.php
@@ -19,38 +19,39 @@ abstract class AbstractProcessor
 		}
 
 		R::begin();
-		$context = new ProcessingContext();
-		$context->key = $key;
-		$this->beforeProcessing($context);
 
-		$subProcessors = $this->getSubProcessors();
-		$urlMap = [];
-		$urks = [];
-		foreach ($subProcessors as $processor)
-		{
-			foreach ($processor->getURLs($key) as $url)
-			{
-				if (!isset($urlMap[$url]))
-				{
-					$urlMap[$url] = [];
-				}
-				$urlMap[$url] []= $processor;
-				$urls[$url] = $url;
-			}
-		}
-
-		$documents = Downloader::downloadMulti($urls);
-
-		foreach ($documents as &$document)
-		{
-			//別ハックは、	Another hack
-			//私は静かに	makes me
-			//泣きます		quietly weep
-			$document->content = '<?xml encoding="utf-8" ?'.'>' . $document->content;
-		}
-
+		$urls = [];
 		try
 		{
+			$context = new ProcessingContext();
+			$context->key = $key;
+			$this->beforeProcessing($context);
+
+			$subProcessors = $this->getSubProcessors();
+			$urlMap = [];
+			foreach ($subProcessors as $processor)
+			{
+				foreach ($processor->getURLs($key) as $url)
+				{
+					if (!isset($urlMap[$url]))
+					{
+						$urlMap[$url] = [];
+					}
+					$urlMap[$url] []= $processor;
+					$urls[$url] = $url;
+				}
+			}
+
+			$documents = Downloader::downloadMulti($urls);
+
+			foreach ($documents as &$document)
+			{
+				//別ハックは、	Another hack
+				//私は静かに	makes me
+				//泣きます		quietly weep
+				$document->content = '<?xml encoding="utf-8" ?'.'>' . $document->content;
+			}
+
 			foreach ($subProcessors as $subProcessor)
 			{
 				$subDocuments = [];
