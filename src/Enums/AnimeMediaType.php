9d70536ac392c3a4ff132e3ff50d7c32ec44547b
Marcin Kurczewski <mkurczew@gmail.com>
2013-08-05 14:40:05 +0200

    Added toString() method

--- src/Enums/AnimeMediaType.php
+++ src/Enums/AnimeMediaType.php
@@ -8,4 +8,25 @@ class AnimeMediaType extends MediaType
 	const Special = 4;
 	const ONA = 5;
 	const Music = 6;
+
+	public static function toString($type, $media = null)
+	{
+		switch ($type)
+		{
+			case self::TV:
+				return 'TV';
+			case self::OVA:
+				return 'OVA';
+			case self::Movie:
+				return 'movie';
+			case self::Special:
+				return 'special';
+			case self::ONA:
+				return 'ONA';
+			case self::Music:
+				return 'music';
+			default:
+				return 'unknown';
+		}
+	}
 }
--- src/Enums/MangaMediaType.php
+++ src/Enums/MangaMediaType.php
@@ -9,4 +9,27 @@ class MangaMediaType extends MediaType
 	const Manhwa = 5;
 	const Manhua = 6;
 	const OEL = 7;
+
+	public static function toString($type, $media = null)
+	{
+		switch ($type)
+		{
+			case self::Manga:
+				return 'manga';
+			case self::Novel:
+				return 'novel';
+			case self::OneShot:
+				return 'one shot';
+			case self::Doujin:
+				return 'doujin';
+			case self::Manhwa:
+				return 'manhwa';
+			case self::Manhua:
+				return 'manhua';
+			case self::OEL:
+				return 'OEL';
+			default:
+				return 'unknown';
+		}
+	}
 }
