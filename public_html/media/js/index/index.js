89dfd8aea537ca6d7811f77ceb9d1c14ae58df37
Marcin Kurczewski <mkurczew@gmail.com>
2013-08-10 12:03:16 +0200

    Changes to layout; closed #42

--- public_html/media/js/index/index.js
+++ public_html/media/js/index/index.js
@@ -1,8 +1,32 @@
+var lastNum = 0;
+
+function switchShowcaseTab(num)
+{
+	$('#showcase menu li').removeClass('active');
+	$('#showcase menu li').eq(num).addClass('active');
+	if (lastNum == num)
+	{
+		return;
+	}
+	lastNum = num;
+	$('#showcase .tab').slideUp('fast');
+	$('#showcase .tab').eq(num).slideDown('fast');
+}
+
 $(function()
 {
 	$('#main .search input').focus();
-	$('a.about-mal').click(function(event) {
-		$('div.about-mal').fadeToggle();
-		event.preventDefault();
+
+	$('#showcase menu li').each(function(i, index)
+	{
+		$(this).hover(function()
+		{
+			switchShowcaseTab(i);
+		}).click(function(e)
+		{
+			e.preventDefault();
+		});
 	});
+
+	switchShowcaseTab(0);
 });
