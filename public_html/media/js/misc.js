1b9ce8106d1f1f1c953fbc2876f1b52668e42658
Marcin Kurczewski <mkurczew@gmail.com>
2013-08-10 12:04:52 +0200

    Removed autofocus due to problems on mobile devies
    Making autofocus on search input on every page caused mobile devices to show
    keyboard on such pages, which isn't very user-friendly. This commit leaves
    autofocus on main page and /list pages.

--- public_html/media/js/misc.js
+++ public_html/media/js/misc.js
@@ -5,13 +5,6 @@ $.fn.hasAttr = function(name)
 
 $(function()
 {
-	// scroll scrollable elements
-	//$('.scrollable').jScrollPane({horizontalDragMaxWidth: 0, autoReinitialise: true});
-	// focus user search field
-	if ($('input:focus').length == 0)
-	{
-		$('#header input').focus();
-	}
 	$('.search').submit(function(e)
 	{
 		var userName = $(this).find('[name=user-name]').val();
