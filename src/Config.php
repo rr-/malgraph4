d93863fa9acf2229d5f19d7998cb5940aa2c72e9
Marcin Kurczewski <mkurczew@gmail.com>
2013-08-29 08:53:10 +0200

    Smart host location retrieval

--- src/Config.php
+++ src/Config.php
@@ -50,7 +50,7 @@ class Config extends Singleton
 		self::$mirrorEnabled = false;
 		self::$cacheEnabled = true;
 		self::$cacheTimeToLive = 24 * 60 * 60;
-		self::$baseUrl = 'http://mal.oko.im/';
+		self::$baseUrl = 'http://' . $_SERVER['HTTP_HOST'] . '/';
 		self::$googleAdsEnabled = true;
 		self::$googleAnalyticsEnabled = true;
 		self::$sendReferrer = true;
