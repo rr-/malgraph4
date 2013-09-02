225c0a80a4ccf9aae75b4209d2ea82803ebb7cc9
Marcin Kurczewski <mkurczew@gmail.com>
2013-09-02 08:36:55 +0200

    Fixed #112

--- src/ModelUtils/Distributions/MediaYearDistribution.php
+++ src/ModelUtils/Distributions/MediaYearDistribution.php
@@ -29,7 +29,7 @@ class MediaYearDistribution extends AbstractDistribution
 
 	public function getNullGroupKey()
 	{
-		return 0;
+		return null;
 	}
 
 	public static function getPublishedYear($entry)
