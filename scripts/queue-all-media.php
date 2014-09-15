838c09b2edd684cd4a0aef2c3f02058c504d689f
rr- <mkurczew@gmail.com>
2014-09-15 11:20:29 +0200

    Added malformed media handling to enqueue script

--- scripts/queue-all-media.php
+++ scripts/queue-all-media.php
@@ -3,12 +3,21 @@ require_once __DIR__ . '/../src/core.php';
 
 $mediaQueue = new Queue(Config::$mediaQueuePath);
 
-$query = 'SELECT media, mal_id FROM media';
+$query = 'SELECT id, media, mal_id FROM media';
 $media = R::getAll($query);
 
 $mediaIds = [];
 foreach ($media as $entry)
-	$mediaIds []= TextHelper::serializeMediaId($entry);
+{
+	try
+	{
+		$mediaIds []= TextHelper::serializeMediaId($entry);
+	}
+	catch (InvalidArgumentException $e)
+	{
+		echo 'Media ' . print_r($entry) . ' cannot be serialized';
+	}
+}
 
 $mediaQueue->enqueueMultiple(array_map(function($mediaId)
 	{
